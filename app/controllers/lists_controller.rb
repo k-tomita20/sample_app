6da6d6b7259a0de1e3e0c22e3cebc0e33c97f3a9
k-tomita20 <208903862+k-tomita20@users.noreply.github.com>
2025-05-14 13:01:22 +0000

    [finish] 3-5

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -17,7 +17,14 @@ class ListsController < ApplicationController
     @list = List.find(params[:id])
   end
 
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to list_path(list.id)
+  end
+
   def edit
+    @list = List.find(params[:id])
   end
 
   private
